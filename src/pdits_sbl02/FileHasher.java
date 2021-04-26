8ff9fae03482c4da1328bdb38a3474860eb92a1a
philippschwabenbauer <philipp.schwabenbauer@gmx.de>
2021-04-26 11:06:55 +0200

    FileHasher überarbeitet

--- src/pdits_sbl02/FileHasher.java
+++ src/pdits_sbl02/FileHasher.java
@@ -2,59 +2,74 @@ package pdits_sbl02;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.lang.System.Logger;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.util.Scanner;
-import java.util.logging.FileHandler;
 
 /**
  * Aufgabe 4 "FileHasher" der "Praxis der IT-Sicherheit"-SBL02.
  */
+
 public class FileHasher {
 	// path of file to hash
 	private static final String FILE_PATH = "./res/n01.txt.enc";
 
-	public static void main(String[] args) {
-		// read in the algorithm the user wants to use
+	// Get the algorithm the user wants to use (SHA-256, SHA-512, MD5)
+	private static String getAlgorithm(){
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter Algorithm (SHA-256, SHA-512, MD5): ");
 		String algorithm = scanner.nextLine();
 		scanner.close();
+		return algorithm;
+	}
+
+	// convert byte array hash to hexadecimal string
+	private static String toHexString(byte[] hash){
+		String hashHexString = "";
+
+		for (int i = 0; i < hash.length; i++) {
+			hashHexString += String.format("%02X", hash[i]);
+		}
+		return hashHexString;
+	}
+
+	// write hash value to file
+	private static void writeHashToFile(String hash, String algorithm){
+		String fileName = algorithm + "-Hash.log";
+		BufferedWriter bw = null;
 
 		try {
-			// read cipher file as byte array
-			byte[] file = Files.readAllBytes(Path.of(FILE_PATH));
+			bw = new BufferedWriter(new FileWriter(fileName));
+			bw.write(hash);
+			bw.close();
+			// print filename
+			System.out.println(algorithm + "-Hash value written in file \"" + fileName + "\"");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		try {
+			String hashHexString = "";
+			String algorithm = getAlgorithm();
 
 			// create instance of hasher of the algorithm
 			MessageDigest hasher = MessageDigest.getInstance(algorithm);
 
-			// create the hash value of the file
-			byte[] hash = hasher.digest(file);
+			// read file as byte array and generate the hash value of the file
+			byte[] hash = hasher.digest(Files.readAllBytes(Path.of(FILE_PATH)));
+			hashHexString = toHexString(hash);
 
-			String hashHexString = "";
-			System.out.print("Hash of file \"" + FILE_PATH + "\" with " + algorithm + ": ");
-			System.out.println();
+			System.out.println("Hash of file \"" + FILE_PATH + "\" with " + algorithm + ": ");
+			System.out.println(hashHexString);
 
-			// convert hash to hex string
-			for (int i = 0; i < hash.length; i++) {
-				hashHexString += String.format("%02X", hash[i]);
-			}
+			// write hash value to file
+			writeHashToFile(hashHexString, algorithm);
 
-			// print the hash
-			System.out.print(hashHexString);
-			System.out.println();
-
-			String fileName = algorithm + "-Hash.log";
-
-			// write hash to file
-			BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
-			bw.write(hashHexString);
-			bw.close();
-
-			// print filename
-			System.out.println(algorithm + "-Hash value written in file \"" + fileName + "\"");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
